db4e6f64859986264fb1a75bb0d80bced8a22064
tran.ba.thiet <tran.ba.thiet@vn-macbook-0086s-mac-mini.local>
2018-03-23 11:09:08 +0700

    Initial Commit

---
+++ BaseProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BaseProject
+//
+//  Created by tran.ba.thiet on 3/23/18.
+//  Copyright © 2018 tran.ba.thiet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
